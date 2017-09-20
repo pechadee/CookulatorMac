a23744b48900f8bf969b893322b55dec47b7addf
Petra Dueck <petra@node-1w7jr9qkaqait115bv6v3orhn.ipv6.telus.net>
2017-09-20 10:43:00 -0700

    Initial Commit

---
+++ CookulatorMac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CookulatorMac
+//
+//  Created by Petra Dueck on 2017-09-20.
+//  Copyright © 2017 Petra Dueck. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
